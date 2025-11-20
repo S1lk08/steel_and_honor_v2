85939285c3e3bbe2d5caf4883a5fcb4f1a5af665
S1lk08 <s1lkgamingmc@gmail.com>
2025-11-20 08:27:07 -0600

    Update BannerPreviewWidget.kt

--- src/client/kotlin/com/steelandhonor/modid/client/gui/BannerPreviewWidget.kt
+++ src/client/kotlin/com/steelandhonor/modid/client/gui/BannerPreviewWidget.kt
@@ -1,6 +1,5 @@
 package com.steelandhonor.modid.client.gui
 
-import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.gui.widget.ClickableWidget
 import net.minecraft.item.ItemStack
@@ -10,11 +9,9 @@ class BannerPreviewWidget(
     private val banner: ItemStack
 ) : ClickableWidget(x, y, 20, 40, banner.name) {
 
-    override fun render(context: DrawContext, mouseX: Int, mouseY: Int, delta: Float) {
+    override fun renderWidget(context: DrawContext, mouseX: Int, mouseY: Int, delta: Float) {
         if (!banner.isEmpty) {
-            val itemRenderer = MinecraftClient.getInstance().itemRenderer
-            itemRenderer.renderInGui(context, banner, x, y)
+            context.drawItem(banner, x, y)
         }
-        super.render(context, mouseX, mouseY, delta)
     }
 }
