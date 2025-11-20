fa23a4e8dc8eb8ec87d36721b67ac09763180d82
S1lk08 <s1lkgamingmc@gmail.com>
2025-11-20 08:31:20 -0600

    Update BannerPreviewWidget.kt

--- src/client/kotlin/com/steelandhonor/modid/client/gui/BannerPreviewWidget.kt
+++ src/client/kotlin/com/steelandhonor/modid/client/gui/BannerPreviewWidget.kt
@@ -2,22 +2,27 @@ package com.steelandhonor.modid.client.gui
 
 import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.gui.widget.ClickableWidget
-import net.minecraft.client.gui.narration.NarrationMessageBuilder
-import net.minecraft.client.gui.narration.NarrationPart
+import net.minecraft.client.gui.screen.narration.NarrationElementOutput
 import net.minecraft.item.ItemStack
 
 class BannerPreviewWidget(
-    x: Int, y: Int,
+    x: Int,
+    y: Int,
     private val banner: ItemStack
 ) : ClickableWidget(x, y, 20, 40, banner.name) {
 
-    override fun renderWidget(context: DrawContext, mouseX: Int, mouseY: Int, delta: Float) {
+    override fun renderWidget(
+        context: DrawContext,
+        mouseX: Int,
+        mouseY: Int,
+        delta: Float
+    ) {
         if (!banner.isEmpty) {
             context.drawItem(banner, x, y)
         }
     }
 
-    override fun appendClickableNarrations(builder: NarrationMessageBuilder) {
-        builder.put(NarrationPart.TITLE, this.message)
+    override fun appendClickableNarrations(output: NarrationElementOutput) {
+        output.add(this.message)
     }
 }
